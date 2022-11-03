ab71e27d62e365c787e5101f8acfb99efa305dcc
EL-Q <coderelq357@gmail.com>
2022-11-03 16:16:24 +0800

    belajar py dasar

---
+++ BASIC/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for info in kelompok:
+#     print(f"NAMA : {info [0]}")
+#     print(f"NIM  : {info [1]}")
+#     print(f"PRODI: {info [2]}\n")
---
+++ python.py
@@ -0,0 +1,10 @@
+"""
+alur belajar python
+1.py dasar
+2.py oop
+3.py restframework
+
+-git hub
+git remote -v : melihat origirn aktif
+git push origin master : mengepus oke branc master
+"""
